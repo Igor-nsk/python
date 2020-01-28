5571ed704a5d64dda30a74d92932f71ac736a22d
Igor-nsk <terekhov.ig.87@gmail.com>
2020-01-29 00:11:00 +0700

    add new branch

--- send email.py
+++ send email.py
@@ -5,11 +5,12 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time
-from selenium import webdriver
+import unittest, time, re
 
 class AppDynamicsJob(unittest.TestCase):
     def setUp(self):
+        # AppDynamics will automatically override this web driver
+        # as documented in https://docs.appdynamics.com/display/PRO44/Write+Your+First+Script
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
         self.base_url = "https://www.google.com/"
@@ -18,16 +19,17 @@ class AppDynamicsJob(unittest.TestCase):
     
     def test_app_dynamics_job(self):
         driver = self.driver
-        # open home page
         driver.get("https://mail.ru/")
-        self.login(driver)
-        self.send_email(driver)
-
+        driver.find_element_by_id("mailbox:login").click()
+        driver.find_element_by_id("mailbox:login").click()
+        driver.find_element_by_xpath(u"//input[@value='Ввести пароль']").click()
+        driver.find_element_by_id("mailbox:password").click()
+        driver.find_element_by_id("mailbox:password").clear()
+        driver.find_element_by_id("mailbox:password").send_keys("igi1987")
+        driver.find_element_by_xpath(u"//input[@value='Ввести пароль']").click()
         driver.find_element_by_id("PH_logoutLink").click()
-
-    def send_email(self, driver):
-        driver.find_element_by_css_selector(
-            "css=.compose-button__txt").click()
+        driver.find_element_by_xpath("//div[@id='app-canvas']/div/div/div/div/div[2]/div/div/div/div/div/div/span/span/span").click()
+        # ERROR: Caught exception [unknown command [editContent]]
         driver.find_element_by_xpath("(//input[@value=''])[12]").click()
         driver.find_element_by_xpath("(//input[@value='pithonich@mail.ru'])[2]").clear()
         driver.find_element_by_xpath("(//input[@value='pithonich@mail.ru'])[2]").send_keys("pithonich@mail.ru")
@@ -36,22 +38,12 @@ class AppDynamicsJob(unittest.TestCase):
         driver.find_element_by_name("Subject").send_keys("hello")
         driver.find_element_by_xpath("(//button[@type='button'])[17]").click()
         driver.find_element_by_xpath("//div[5]/div/div/div[2]/div/div/div").click()
+        # ERROR: Caught exception [unknown command [editContent]]
         driver.find_element_by_xpath("//div[2]/div/span/span/span").click()
         driver.find_element_by_css_selector("svg.ico.ico_16-close.ico_size_s").click()
-        driver.find_element_by_xpath(
-            "//div[@id='app-canvas']/div/div/div/div/div[2]/div[2]/div/div/div/div/div/div/div/div/div/a/div[4]/div/div/span").click()
-        driver.find_element_by_xpath(
-            "//div[@id='app-canvas']/div/div/div/div/div[2]/div/div/div/div/div[2]/div/div/nav/a/div/div[2]/div").click()
-
-    def login(self, driver, username="pithonich"):
-        driver.find_element_by_id("mailbox:login").click()
-        driver.find_element_by_id("mailbox:login").send_keys("%s" % username)
-        driver.find_element_by_xpath(u"//input[@value='Ввести пароль']").click()
-        driver.find_element_by_id("mailbox:password").clear()
-        driver.find_element_by_id("mailbox:password").send_keys("gbnjysxhekbnn")
-        driver.find_element_by_xpath(u"//input[@value='Ввести пароль']").click()
-        time.sleep(4)
-
+        driver.find_element_by_xpath("//div[@id='app-canvas']/div/div/div/div/div[2]/div[2]/div/div/div/div/div/div/div/div/div/a/div[4]/div/div/span").click()
+        driver.find_element_by_xpath("//div[@id='app-canvas']/div/div/div/div/div[2]/div/div/div/div/div[2]/div/div/nav/a/div/div[2]/div").click()
+    
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
